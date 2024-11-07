70440e9f5036a3d3bd240fb181b2a27cb267ee50
JMVCoelho <joao.vares.coelho@tecnico.ulisboa.pt>
2024-11-06 22:33:16 -0500

    mates and less

--- scripts/less_influence.py
+++ scripts/less_influence.py
@@ -1,10 +1,13 @@
 import torch
 from transformers import AutoTokenizer
 import random
-from tevatron.retriever.modeling import DenseModel
+from tevatron.retriever.modeling import DenseModel, DenseModelWithNegativeCache
 
-from tevatron.retriever.arguments import ModelArguments, DataArguments, \
-    TevatronTrainingArguments as TrainingArguments
+from tevatron.retriever.arguments import (
+    ModelArguments,
+    DataArguments,
+    TevatronTrainingArguments as TrainingArguments,
+)
 
 import torch.nn.functional as F
 from datasets import load_dataset
@@ -12,6 +15,7 @@ from datasets import load_dataset
 import os
 
 import logging
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
@@ -20,7 +24,7 @@ logging.basicConfig(
 )
 
 import pickle
-from tqdm import tqdm 
+from tqdm import tqdm
 
 from transformers import (
     HfArgumentParser,
@@ -29,9 +33,17 @@ from transformers import (
 import sys
 
 
-class LESSQueryAttribution():
-    def __init__(self, valid_gradient_path, optim_state_path, model_args, training_args, data_args):
-        
+class LESSQueryAttribution:
+    def __init__(
+        self,
+        valid_gradient_path,
+        optim_state_path,
+        use_negative_cache,
+        model_args,
+        training_args,
+        data_args,
+    ):
+
         self.data_args = data_args
 
         self.tokenizer = AutoTokenizer.from_pretrained(
@@ -41,90 +53,171 @@ class LESSQueryAttribution():
 
         if self.tokenizer.pad_token_id is None:
             self.tokenizer.pad_token_id = self.tokenizer.unk_token_id
-        self.tokenizer.padding_side = 'right'
+        self.tokenizer.padding_side = "right"
 
-        self.validation_gradient = self.get_validation_gradient(valid_gradient_path).cuda()
-        optimizer_states = torch.load(optim_state_path, map_location='cpu')
-        self.avg = optimizer_states['optimizer_state_dict']['optimizer_state_dict']['state'][0]['exp_avg'].cuda()
-        self.avg_sq = optimizer_states['optimizer_state_dict']['optimizer_state_dict']['state'][0]['exp_avg_sq'].cuda()
+        self.validation_gradient = self.get_validation_gradient(
+            valid_gradient_path
+        ).cuda()
+        optimizer_states = torch.load(optim_state_path, map_location="cpu")
+        self.avg = optimizer_states["optimizer_state_dict"]["optimizer_state_dict"][
+            "state"
+        ][0]["exp_avg"].cuda()
+        self.avg_sq = optimizer_states["optimizer_state_dict"]["optimizer_state_dict"][
+            "state"
+        ][0]["exp_avg_sq"].cuda()
 
         del optimizer_states
 
-        self.model = DenseModel.build(
-            model_args,
-            training_args,
-            cache_dir=model_args.cache_dir,
-            )
-
-        self.model.to("cuda")
-
         data_files = [f"en_{str(i).zfill(2)}.jsonl" for i in range(24)]
-        self.dataset = load_dataset("XBKYS/minicpm-embedding-data", data_files=data_files, split="train", cache_dir=data_args.dataset_cache_dir)
+        self.dataset = load_dataset(
+            "XBKYS/minicpm-embedding-data",
+            data_files=data_files,
+            split="train",
+            cache_dir=data_args.dataset_cache_dir,
+        )
         print(f"Sharding in {data_args.dataset_number_of_shards} shards.")
         print(f"Processing shard {data_args.dataset_shard_index}.")
-        self.dataset = self.dataset.shard(num_shards=data_args.dataset_number_of_shards, index=data_args.dataset_shard_index)
+        self.dataset = self.dataset.shard(
+            num_shards=data_args.dataset_number_of_shards,
+            index=data_args.dataset_shard_index,
+        )
 
-    
+        if not use_negative_cache:
+            self.model = DenseModel.build(
+                model_args,
+                training_args,
+                cache_dir=model_args.cache_dir,
+            )
+
+            self.model.to("cuda")
+
+        else:
+            logger.info("Using Negative Cache")
+            self.model = DenseModelWithNegativeCache.build(
+                model_args,
+                training_args,
+                cache_dir=model_args.cache_dir,
+            )
+
+            self.model.to("cuda")
+
+            self.build_negative_cache()
+
     def set_seed(self, seed):
         random.seed(seed)
-        
+
+    def build_negative_cache(
+        self, batch_size=300, cache_size=2874
+    ):  # (8gpu * 60 batch size * 6 docs/query) - 6
+        self.model.eval()
+
+        docs = []
+        while len(docs) < cache_size:
+            random_index = random.randint(0, len(self.dataset) - 1)
+            random_negative_set = self.dataset[random_index]["neg"]
+            random_negative_index = random.randint(
+                1, len(random_negative_set) - 1
+            )  # 0th is a prompt
+            docs.append(random_negative_set[random_negative_index])
+
+        all_reps = []
+        with torch.no_grad():
+            for i in range(0, len(docs), batch_size):
+                batch_docs = docs[i : i + batch_size]
+
+                with torch.cuda.amp.autocast(dtype=torch.bfloat16):
+                    _, tokenized_docs = self.tokenize(q=None, d=batch_docs)
+                    tokenized_docs = {
+                        k: v.to("cuda") for k, v in tokenized_docs.items()
+                    }
+                    batch_reps = self.model(passage=tokenized_docs).p_reps.detach()
+                    all_reps.append(batch_reps)
+
+        cache_reps = torch.cat(all_reps, dim=0)
+        assert (
+            cache_reps.size(0) == cache_size
+        ), f"Cache size mismatch: {cache_reps.size(0)} vs {cache_size}"
+        self.model.init_cache(cache_reps)
+        self.model.train()
 
     def get_validation_gradient(self, valid_gradient_path):
-        with open(valid_gradient_path, 'rb') as h:
+        with open(valid_gradient_path, "rb") as h:
             grad = pickle.load(h).float()
-        
-        print(f"Loaded valid gradient. Shape: {grad.shape}. Norm: {torch.norm(grad, p=2).item()}")
+
+        print(
+            f"Loaded valid gradient. Shape: {grad.shape}. Norm: {torch.norm(grad, p=2).item()}"
+        )
         return grad
 
     def tokenize(self, q, d):
 
+        if q is not None:
             q_collated = self.tokenizer(
                 q,
-                padding=False, 
+                padding=False,
                 truncation=True,
-                max_length=self.data_args.query_max_len-1 if self.data_args.append_eos_token else self.data_args.query_max_len,
-                return_attention_mask=False,
-                return_token_type_ids=False,
-                add_special_tokens=True,
-            )
-            d_collated = self.tokenizer(
-                d,
-                padding=False, 
-                truncation=True,
-                max_length=self.data_args.passage_max_len-1 if self.data_args.append_eos_token else self.data_args.passage_max_len,
+                max_length=(
+                    self.data_args.query_max_len - 1
+                    if self.data_args.append_eos_token
+                    else self.data_args.query_max_len
+                ),
                 return_attention_mask=False,
                 return_token_type_ids=False,
                 add_special_tokens=True,
             )
 
             if self.data_args.append_eos_token:
-                q_collated['input_ids'] = [q + [self.tokenizer.eos_token_id] for q in q_collated['input_ids']]
-                d_collated['input_ids'] = [d + [self.tokenizer.eos_token_id] for d in d_collated['input_ids']]
-            
+                q_collated["input_ids"] = [
+                    q + [self.tokenizer.eos_token_id] for q in q_collated["input_ids"]
+                ]
+
             q_collated = self.tokenizer.pad(
                 q_collated,
-                padding=True, 
+                padding=True,
                 pad_to_multiple_of=self.data_args.pad_to_multiple_of,
                 return_attention_mask=True,
-                return_tensors='pt',
+                return_tensors="pt",
             )
+        else:
+            q_collated = None
+
+        if d is not None:
+            d_collated = self.tokenizer(
+                d,
+                padding=False,
+                truncation=True,
+                max_length=(
+                    self.data_args.passage_max_len - 1
+                    if self.data_args.append_eos_token
+                    else self.data_args.passage_max_len
+                ),
+                return_attention_mask=False,
+                return_token_type_ids=False,
+                add_special_tokens=True,
+            )
+
+            d_collated["input_ids"] = [
+                d + [self.tokenizer.eos_token_id] for d in d_collated["input_ids"]
+            ]
+
             d_collated = self.tokenizer.pad(
                 d_collated,
-                padding=True, 
+                padding=True,
                 pad_to_multiple_of=self.data_args.pad_to_multiple_of,
                 return_attention_mask=True,
-                return_tensors='pt',
+                return_tensors="pt",
             )
-            return q_collated, d_collated
+        else:
+            d_collated = None
+
+        return q_collated, d_collated
 
     def get_grad_dotp(self, q, d, do_adam=False):
 
-        
-        
-        with torch.cuda.amp.autocast(dtype=torch.bfloat16): #HACK hardcoded to bf16
+        with torch.cuda.amp.autocast(dtype=torch.bfloat16):  # HACK hardcoded to bf16
 
-            q = {k:v.view(1, -1).to("cuda") for k, v in q.items()}
-            d = {k:v.to("cuda") for k, v in d.items()}
+            q = {k: v.view(1, -1).to("cuda") for k, v in q.items()}
+            d = {k: v.to("cuda") for k, v in d.items()}
 
             loss = self.model(q, d).loss
 
@@ -134,10 +227,8 @@ class LESSQueryAttribution():
             for param in self.model.parameters():
                 if param.grad is not None:
                     gradients.append(param.grad.detach().view(-1))
-            
 
-            gradient_vector = torch.cat(gradients) 
-            grad_norm = torch.norm(gradient_vector, p=2).item()
+            gradient_vector = torch.cat(gradients)
 
             if do_adam:
                 beta1 = 0.9
@@ -145,26 +236,23 @@ class LESSQueryAttribution():
                 eps = 1e-08
 
                 updated_avg = beta1 * self.avg + (1 - beta1) * gradient_vector
-                updated_avg_sq = beta2 * self.avg_sq + (1 - beta2) * gradient_vector ** 2
+                updated_avg_sq = beta2 * self.avg_sq + (1 - beta2) * gradient_vector**2
                 gradient_vector = updated_avg / torch.sqrt(updated_avg_sq + eps)
 
+            grad_norm = torch.norm(gradient_vector, p=2).item()
             gradient_vector = F.normalize(gradient_vector, p=2, dim=0)
 
             dot_product = torch.dot(gradient_vector, self.validation_gradient)
-            
+
             self.model.zero_grad()
 
         return dot_product, grad_norm
-    
-    def get_less_score(self, example) -> list[str]:        
-        
+
+    def get_less_score(self, example) -> list[str]:
+
         query_text = example["query"][1]
         positive_text = example["pos"][1]
         negative_texts = example["neg"][1:]
-
-        # random_index = random.randint(0, len(self.dataset) - 1)
-        # random_example = self.dataset[random_index]
-        # more_negatives = random_example["neg"][1:]
 
         queries = [query_text]
         documents = [positive_text] + negative_texts
@@ -174,24 +262,31 @@ class LESSQueryAttribution():
         dot_prod, grad_norm = self.get_grad_dotp(q, d)
 
         return dot_prod.item(), grad_norm
-    
+
     def run(self, outpath):
         logger.info(f"Sampling started. Saving to: {outpath}")
-        
-        with open(outpath, 'w') as h1:
-                for example in tqdm(self.dataset):
 
-                    query =  example["query"][1].replace("\t", "").replace("\n", "").replace("\r", "")
-                    
-                    dot_product, grad_norm = self.get_less_score(example)
-                    h1.write(f"{query}\t{dot_product}\t{grad_norm}\n")
+        with open(outpath, "w") as h1:
+            for example in tqdm(self.dataset):
+
+                query = (
+                    example["query"][1]
+                    .replace("\t", "")
+                    .replace("\n", "")
+                    .replace("\r", "")
+                )
+
+                dot_product, grad_norm = self.get_less_score(example)
+                h1.write(f"{query}\t{dot_product}\t{grad_norm}\n")
 
 
 def main():
     parser = HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
 
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
-        model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+        model_args, data_args, training_args = parser.parse_json_file(
+            json_file=os.path.abspath(sys.argv[1])
+        )
     else:
         model_args, data_args, training_args = parser.parse_args_into_dataclasses()
         model_args: ModelArguments
@@ -201,13 +296,17 @@ def main():
     attribution_method = LESSQueryAttribution(
         valid_gradient_path="/data/user_data/jmcoelho/models/Qwen2.5-0.5B-bidirectional-attn-wavg-pool-mntp-minicpmembed-random-20k-1gpu-valid-grads/normalized_final_valid_grad.pkl",
         optim_state_path="/data/user_data/jmcoelho/models/Qwen2.5-0.5B-bidirectional-attn-wavg-pool-mntp-minicpmembed-random-20k-1gpu/checkpoint-167/global_step167/bf16_zero_pp_rank_0_mp_rank_00_optim_states.pt",
+        use_negative_cache=True,
         model_args=model_args,
         data_args=data_args,
-        training_args=training_args
+        training_args=training_args,
     )
 
-    attribution_method.run(outpath=f"{training_args.output_dir}_{data_args.dataset_shard_index}")
+    attribution_method.set_seed(17121998)
+    attribution_method.run(
+        outpath=f"{training_args.output_dir}_{data_args.dataset_shard_index}"
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
--- src/tevatron/retriever/dataset.py
+++ src/tevatron/retriever/dataset.py
@@ -11,20 +11,24 @@ from tevatron.retriever.arguments import DataArguments
 from transformers import BatchEncoding
 
 import logging
+
 logger = logging.getLogger(__name__)
 
 
-def format_query(query: str, prefix: str = '') -> str:
-    return f'{prefix} {query.strip()}'.strip()
+def format_query(query: str, prefix: str = "") -> str:
+    return f"{prefix} {query.strip()}".strip()
 
-def format_passage(text: str, title: str = '', prefix: str = '', add_markers: bool = False) -> str:
+
+def format_passage(
+    text: str, title: str = "", prefix: str = "", add_markers: bool = False
+) -> str:
     if add_markers:
-        return f'{prefix} Title: {title.strip()} Text: {text.strip()}'.strip()
+        return f"{prefix} Title: {title.strip()} Text: {text.strip()}".strip()
     else:
-        return f'{prefix} {title.strip()} {text.strip()}'.strip()
+        return f"{prefix} {title.strip()} {text.strip()}".strip()
 
 
-class Cropping():
+class Cropping:
     def __init__(self, ratio_min: float = 0.1, ratio_max: float = 0.5):
         self.ratio_min = ratio_min
         self.ratio_max = ratio_max
@@ -35,15 +39,15 @@ class Cropping():
         length = int(len(words) * ratio)
         start = random.randint(0, len(words) - length)
         end = start + length
-        cropped_words = words[start:end] 
-        return ' '.join(cropped_words)  
+        cropped_words = words[start:end]
+        return " ".join(cropped_words)
 
     def __call__(self, data: List[int]) -> List[int]:
         return self.augment(data)
-    
+
 
 class PretrainDataset(Dataset):
-    def __init__(self, data_args: DataArguments, trainer = None):
+    def __init__(self, data_args: DataArguments, trainer=None):
         self.data_args = data_args
         self.train_data = load_dataset(
             self.data_args.dataset_name,
@@ -61,22 +65,22 @@ class PretrainDataset(Dataset):
 
         self.strategy = Cropping()
 
-
     def __len__(self):
         return len(self.train_data)
 
     def __getitem__(self, item) -> Tuple[str, List[str]]:
         group = self.train_data[item]
 
-        content = f"{group['title']} {group['text']}" # assumes dataset is a jsonl with title and text fields
+        content = f"{group['title']} {group['text']}"  # assumes dataset is a jsonl with title and text fields
 
         formated_query = self.strategy(content)
         formated_passages = [self.strategy(content)]
 
         return formated_query, formated_passages
-    
+
+
 class TrainDataset(Dataset):
-    def __init__(self, data_args: DataArguments, trainer = None):
+    def __init__(self, data_args: DataArguments, trainer=None):
         self.data_args = data_args
         self.train_data = load_dataset(
             self.data_args.dataset_name,
@@ -101,9 +105,9 @@ class TrainDataset(Dataset):
 
         _hashed_seed = hash(item + self.trainer.args.seed)
 
-        query = group['query']
-        group_positives = group['positive_passages']
-        group_negatives = group['negative_passages']
+        query = group["query"]
+        group_positives = group["positive_passages"]
+        group_negatives = group["negative_passages"]
 
         formated_query = format_query(query, self.data_args.query_prefix)
         formated_passages = []
@@ -112,8 +116,12 @@ class TrainDataset(Dataset):
             pos_psg = group_positives[0]
         else:
             pos_psg = group_positives[(_hashed_seed + epoch) % len(group_positives)]
-        
-        formated_passages.append(format_passage(pos_psg['text'], pos_psg['title'], self.data_args.passage_prefix))
+
+        formated_passages.append(
+            format_passage(
+                pos_psg["text"], pos_psg["title"], self.data_args.passage_prefix
+            )
+        )
 
         negative_size = self.data_args.train_group_size - 1
         if len(group_negatives) < negative_size:
@@ -127,30 +135,35 @@ class TrainDataset(Dataset):
             negs = [x for x in group_negatives]
             random.Random(_hashed_seed).shuffle(negs)
             negs = negs * 2
-            negs = negs[_offset: _offset + negative_size]
+            negs = negs[_offset : _offset + negative_size]
 
         for neg_psg in negs:
-            formated_passages.append(format_passage(neg_psg['text'], neg_psg['title'], self.data_args.passage_prefix))
+            formated_passages.append(
+                format_passage(
+                    neg_psg["text"], neg_psg["title"], self.data_args.passage_prefix
+                )
+            )
 
         return formated_query, formated_passages
 
 
 class MiniCPM_UnsupervisedDataset(Dataset):
-    def __init__(self, data_args: DataArguments, trainer = None):
+    def __init__(self, data_args: DataArguments, trainer=None):
         self.data_args = data_args
 
+        # data_files = [f"en_{str(i).zfill(2)}.jsonl" for i in range(24)]
+        # self.train_data = load_dataset("XBKYS/minicpm-embedding-data", data_files=data_files, split="train")
+        # # self.train_data = load_dataset("XBKYS/minicpm-embedding-data", data_files={
+        # #         'train': [f'en_{i:02d}.jsonl' for i in range(24)]
+        # # }, cache_dir=self.data_args.dataset_cache_dir)
 
-        # login("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")
+        # self.train_data = self.train_data.shuffle(seed=42).select(range(180000))
 
-        # self.train_data = load_dataset("XBKYS/minicpm-embedding-data", data_files={
-        #         'train': [f'en_{i:02d}.jsonl' for i in range(24)]
-        # })
-        print("HARDCODED: Loading the filtered version!")
-        self.train_data = load_from_disk("/data/jcoelho/datasets/minicpm_embedding_unsup_queries_2.1M_filtered")
+        print("HARDCODED: Loading a filtered version from disk.")
+        self.train_data = load_from_disk(
+            "/data/user_data/jmcoelho/datasets/minicpm_embedding_unsupervised_queries/mates_15q_6d_multiple_valid"
+        )
 
-        print(self.train_data[0]["bge_ranker_scores"])
-        print(self.train_data[1]["bge_ranker_scores"])
-        print(self.train_data[2]["bge_ranker_scores"])
         if self.data_args.dataset_number_of_shards > 1:
             self.encode_data = self.encode_data.shard(
                 num_shards=self.data_args.dataset_number_of_shards,
@@ -158,35 +171,33 @@ class MiniCPM_UnsupervisedDataset(Dataset):
             )
         self.trainer = trainer
 
-
     def __len__(self):
         try:
-            return len(self.train_data['train'])
+            return len(self.train_data["train"])
         except Exception:
             return len(self.train_data)
 
     def __getitem__(self, item) -> Tuple[str, List[str]]:
         try:
-            group = self.train_data['train'][item]
+            group = self.train_data["train"][item]
         except Exception:
             group = self.train_data[item]
-        
-        epoch = int(self.trainer.state.epoch) if self.trainer.state.epoch is not None else 1
+
+        epoch = (
+            int(self.trainer.state.epoch) if self.trainer.state.epoch is not None else 1
+        )
 
         _hashed_seed = hash(item + self.trainer.args.seed)
 
+        if "positive_passages" in group:
+            query = group["query"]
+            group_positives = group["positive_passages"]
+            group_negatives = group["negative_passages"]
 
-        if 'positive_passages' in group:
-            query = group['query']
-            group_positives = group['positive_passages']
-            group_negatives = group['negative_passages']
-
-        else: # this is for the minicpm data format
-            query = group['query'][1]
-            group_positives = [group['pos'][1]] #!!!!!!!!!!!!!!!!!!!! 
-            group_negatives = group['neg'][1:]
-
-
+        else:  # this is for the minicpm data format
+            query = group["query"][1]
+            group_positives = [group["pos"][1]]  #!!!!!!!!!!!!!!!!!!!!
+            group_negatives = group["neg"][1:]
 
         formated_query = format_query(query, self.data_args.query_prefix)
         formated_passages = []
@@ -195,8 +206,10 @@ class MiniCPM_UnsupervisedDataset(Dataset):
             pos_psg = group_positives[0]
         else:
             pos_psg = group_positives[(_hashed_seed + epoch) % len(group_positives)]
-        
-        formated_passages.append(format_passage(pos_psg, "", self.data_args.passage_prefix))
+
+        formated_passages.append(
+            format_passage(pos_psg, "", self.data_args.passage_prefix)
+        )
 
         negative_size = self.data_args.train_group_size - 1
         if len(group_negatives) < negative_size:
@@ -210,15 +223,18 @@ class MiniCPM_UnsupervisedDataset(Dataset):
             negs = [x for x in group_negatives]
             random.Random(_hashed_seed).shuffle(negs)
             negs = negs * 2
-            negs = negs[_offset: _offset + negative_size]
+            negs = negs[_offset : _offset + negative_size]
 
         for neg_psg in negs:
-            formated_passages.append(format_passage(neg_psg, "", self.data_args.passage_prefix))
+            formated_passages.append(
+                format_passage(neg_psg, "", self.data_args.passage_prefix)
+            )
 
         return formated_query, formated_passages
-    
+
+
 class TrainDatasetPreprocessed(Dataset):
-    def __init__(self, data_args: DataArguments, trainer = None):
+    def __init__(self, data_args: DataArguments, trainer=None, is_eval=False):
         self.data_args = data_args
         self.train_data = load_dataset(
             self.data_args.dataset_name,
@@ -227,6 +243,7 @@ class TrainDatasetPreprocessed(Dataset):
             split=self.data_args.dataset_split,
             cache_dir=self.data_args.dataset_cache_dir,
         )
+
         if self.data_args.dataset_number_of_shards > 1:
             self.encode_data = self.encode_data.shard(
                 num_shards=self.data_args.dataset_number_of_shards,
@@ -235,14 +252,16 @@ class TrainDatasetPreprocessed(Dataset):
         self.trainer = trainer
 
     def create_one_example(self, text_encoding: List[int], is_query=False):
-        max_length = self.data_args.query_max_len if is_query else self.data_args.passage_max_len
+        max_length = (
+            self.data_args.query_max_len if is_query else self.data_args.passage_max_len
+        )
 
         if self.data_args.append_eos_token:
             max_length -= 1
 
         item = self.tokenizer.prepare_for_model(
             text_encoding,
-            truncation='only_first',
+            truncation="only_first",
             max_length=max_length,
             padding=False,
             return_attention_mask=False,
@@ -259,12 +278,12 @@ class TrainDatasetPreprocessed(Dataset):
 
         _hashed_seed = hash(item + self.trainer.args.seed)
 
-        qry = group['query']
+        qry = group["query"]
         encoded_query = self.create_one_example(qry, is_query=True)
 
         encoded_passages = []
-        group_positives = group['positives']
-        group_negatives = group['negatives']
+        group_positives = group["positives"]
+        group_negatives = group["negatives"]
 
         if self.data_args.positive_passage_no_shuffle:
             pos_psg = group_positives[0]
@@ -284,12 +303,13 @@ class TrainDatasetPreprocessed(Dataset):
             negs = [x for x in group_negatives]
             random.Random(_hashed_seed).shuffle(negs)
             negs = negs * 2
-            negs = negs[_offset: _offset + negative_size]
+            negs = negs[_offset : _offset + negative_size]
 
         for neg_psg in negs:
             encoded_passages.append(self.create_one_example(neg_psg))
 
         return encoded_query, encoded_passages
+
 
 class EncodeDataset(Dataset):
 
@@ -314,10 +334,15 @@ class EncodeDataset(Dataset):
     def __getitem__(self, item) -> Tuple[str, str]:
         text = self.encode_data[item]
         if self.data_args.encode_is_query:
-            text_id = text['query_id']
-            formated_text = format_query(text['query'], self.data_args.query_prefix)
+            text_id = text["query_id"]
+            formated_text = format_query(text["query"], self.data_args.query_prefix)
         else:
-            text_id = text['docid'] if 'docid' in text else text['text_id']
-            formated_text = format_passage(text['text'], text['title'], self.data_args.passage_prefix, self.data_args.add_markers)
-        
+            text_id = text["docid"] if "docid" in text else text["text_id"]
+            formated_text = format_passage(
+                text["text"],
+                text["title"],
+                self.data_args.passage_prefix,
+                self.data_args.add_markers,
+            )
+
         return text_id, formated_text
